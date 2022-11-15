8569609d894232e6eaf139bf9af7c1437d62ee89
Replit user <>
2022-11-15 13:03:20 +0000

    lezione2

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print('hello world!')
+my_var = 1
+print('valore {}'.format(my_var))
+hello = None
+stringa = 'Buongiorno al latte ed al caffè'
+print(stringa[10:-1])
+x = 5
+y = 4
+if (4 > 5 or 5 > 4):
+    print('Ciao Domenicoooo')
---
+++ lezione2.py
@@ -0,0 +1,6 @@
+def sum_list(my_list):
+  total = 0
+  for item in my_list:
+    total += item
+  return total
+
